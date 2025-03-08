d910630b1dba0070129d3185ae1a3c7921b3857a
Khumoyun <humoyunortiqov05@gmail.com>
2025-03-08 13:30:35 +0500

    Author serializer created

---
+++ authors/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Author
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Author
+        fields = ['id', 'first_name', 'last_name', 'bio', 'birth_date', 'nationality', 'books_count']
+
+    def get_books_count(self, obj):
+        return obj.books.count()
