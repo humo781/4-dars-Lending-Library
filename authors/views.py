2c2a70b9f17cff4192658db52b2d532f4e8dc6a2
Khumoyun <humoyunortiqov05@gmail.com>
2025-03-09 21:01:53 +0500

    author viewlari toliq yozib bolindi

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,22 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import AuthorSerializer
-from .pagination import AuthorPagination
 from .models import Author
+from books.serializers import BookSerializer
 
 class AuthorListCreateApiView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    pagination_class = AuthorPagination
+
+class AuthorDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class AuthorBookApiView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        author = get_object_or_404(Author, pk=self.kwargs['pk'])
+        return author.books.all()
